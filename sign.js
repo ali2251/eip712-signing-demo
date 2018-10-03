14dca32ef31d90664b4c9a6a33c028d098430f9c
Ali Azam <ali_1632@hotmail.co.uk>
2018-10-03 18:52:43 +0100

    NEVER PUSH NODE MODULES

--- sign.js
+++ sign.js
@@ -40,40 +40,35 @@ window.onload = function (e) {
       { name: "salt", type: "bytes32" },
     ];
 
-    const bid = [
-      { name: "amount", type: "uint256" },
-      { name: "bidder", type: "Identity" },
-    ];
+
 
     const identity = [
-      { name: "userId", type: "uint256" },
-      { name: "wallet", type: "address" },
+      { name: "action", type: "string" },
+      { name: "nonce", type: "uint256" },
+      { name: "wallet", type: "address" }
     ];
 
     const domainData = {
-      name: "My amazing dApp",
-      version: "2",
+      name: "Zinc",
+      version: "1",
       chainId: parseInt(web3.version.network, 10),
       verifyingContract: "0x1C56346CD2A2Bf3202F771f50d3D14a367B48070",
-      salt: "0xf2d857f4a3edcb9b78b4d503bfe733db1e3f6cdc2b7971ee739626c97e86a558"
+      salt: "0x7fea5b30300bd02ca3e22c7837f297a642201dca2185ee2408c2db05f12b94e3"
     };
 
     var message = {
-      amount: 100,
-      bidder: {
-        userId: 323,
-        wallet: "0x3333333333333333333333333333333333333333"
-      }
+      action: "sign up",
+      nonce: 42,
+      wallet: web3.eth.accounts[0]
     };
     
     const data = JSON.stringify({
       types: {
         EIP712Domain: domain,
-        Bid: bid,
-        Identity: identity,
+        Identity: identity
       },
       domain: domainData,
-      primaryType: "Bid",
+      primaryType: "Identity",
       message: message
     });
 
@@ -92,7 +87,7 @@ window.onload = function (e) {
 
         const signature = parseSignature(result.result.substring(2));
 
-        res.style.display = "block";
+        res.style.display = "block"
         res.value = genSolidityVerifier(signature, signer);
       }
     );
